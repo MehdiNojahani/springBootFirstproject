74bb120b4064e93b2a674ea07bb795a8d6f4f083
Mehdi Nojahani <mehdinojahani8183@gmail.com>
2025-05-26 01:59:08 +0330

    Author class as Entity in JPA

---
+++ src/main/java/springboot/learningwithguru/spring6/Author.java
@@ -0,0 +1,43 @@
+package springboot.learningwithguru.spring6;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+
+    //Author attribute :
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String firstName;
+    private String lastName;
+
+
+    //Getter and Setter :
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
