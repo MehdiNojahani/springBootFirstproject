2b0b3877ad7e84dbca1b767a648f1129ab7e65d7
Mehdi Nojahani <mehdinojahani8183@gmail.com>
2025-05-29 02:48:11 +0330

    BootStrapData class implement commandLinerRunner and run methods

--- src/main/java/springboot/learningwithguru/spring6/Spring6Application.java
+++ src/main/java/springboot/learningwithguru/spring6/Spring6Application.java
@@ -2,13 +2,13 @@ package springboot.learningwithguru.spring6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springboot.learningwithguru.spring6.bootstrap.BootStrapData;
 
 @SpringBootApplication
 public class Spring6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring6Application.class, args);
-		System.out.println("Hello Spring Boot");
 	}
 
 }
